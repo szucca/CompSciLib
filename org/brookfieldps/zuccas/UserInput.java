482c0d3cc8afda08ba261167917dadd202634ee4
szucca <scott@zucca.us>
2016-07-13 14:23:31 -0400

    All methods complete
    First version with all input methods. (String/int/double) via console
    and JOptionPane

--- org/brookfieldps/zuccas/UserInput.java
+++ org/brookfieldps/zuccas/UserInput.java
@@ -1,19 +1,40 @@
 package org.brookfieldps.zuccas;
 
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
 /**
- * Write a description of class APCS here.
+ * The UserInput class provides static methods to retrieve input from the user. There are methods for
+ * getting Strings and numbers (doubles or ints) using the console or a JOptionPane dialog box.
  * 
  * @author S. Zucca 
  * @version 2016
  */
 
-import java.util.Scanner;
-import javax.swing.JOptionPane;
-
-public class UserInput
+public final class UserInput
 {
-    private static final String STRING_INPUT_MESSAGE = "Input a String:";
+    /**
+     * Default message for display in any string input method 
+     */
+    private static final String STRING_INPUT_MESSAGE = "Input a String: ";
     
+    /**
+     * Default message for display in any integer input method 
+     */
+    private static final String INT_INPUT_MESSAGE = "Input an integer: ";
+    
+    /**
+     * Default message for display in any double input method 
+     */
+    private static final String DOUBLE_INPUT_MESSAGE = "Input a number: ";
+    
+    private UserInput(){
+        // can not be instantiated
+    }
+    
+    /***
+     * Create a Scanner object and print a message to ensure the console is ready.
+     */
     private static Scanner initScanner(String message){
         Scanner scanner = new Scanner(System.in);
         
@@ -21,26 +42,47 @@ public class UserInput
         return scanner;
     }
     
+    /***
+     * Return a string from the console displaying a custom prompt
+     * 
+     * @param   message     A prompt for the user
+     */
     public static String getString( String message ){
         Scanner scanner = initScanner(message);
         String data = scanner.nextLine();
         return data;
     }
     
-    public static String getString(  ){
+    /***
+     * Return a string from the console displaying default prompt
+     */
+    public static String getString(){
         return getString(STRING_INPUT_MESSAGE);
     }
     
+    /***
+     * Return a string from a dialog box showing a custom prompt (and a standard icon)
+     * 
+     * @param   message     A prompt for the user
+     */
     public static String getStringDialog(String message){
         return JOptionPane.showInputDialog(null, message );
     }
     
+    /***
+     * Return a string from a dialog box showing a default prompt and icon.
+     */
     public static String getStringDialog(){
         return getStringDialog(STRING_INPUT_MESSAGE);
         
     }
     
-    public static int getInteger( String message ){
+    /***
+     * Return an integer from the console with custom prompt.
+     * 
+     * @param message   The custom prompt
+     */
+    public static int getInteger(String message){
         Scanner scanner = initScanner(message);
         Integer n = null;
         
@@ -57,11 +99,49 @@ public class UserInput
         return (int) n;
     }
     
+    /***
+     * Return an integer from the console with default prompt.
+     */
     public static int getInteger(  ){
-        return getInteger("Input an integer:");
+        return getInteger(INT_INPUT_MESSAGE);
     }
     
-    public static double getDouble( String message ){
+    /***
+     * Return an integer from a dialog box showing a default prompt (and a standard icon)
+     */
+    public static int getIntegerDialog(){
+        return getIntegerDialog(INT_INPUT_MESSAGE);
+    }
+            
+    /***
+     * Return an integer from a dialog box showing a custom prompt (and a standard icon)
+     * 
+     * @param   message     A prompt for the user
+     */
+    public static int getIntegerDialog(String message){
+        
+        // get the input and see if it can be made into an integer
+        Integer n = null;
+        String error_msg = "Unable to convert input to an integer. Try again.\n\n" + message;
+        
+        while(n == null) {
+            try {    
+                String input = JOptionPane.showInputDialog(null, message );
+                n = Integer.parseInt(input);
+            } catch(Exception e) {
+                message = error_msg;
+            }
+        }
+        
+        return (int)n;
+    }
+    
+    /***
+     * Return a double from the console with custom prompt.
+     * 
+     * @param message   The custom prompt
+     */
+    public static double getDouble(String message){
         Scanner scanner = initScanner(message);
         Double d = null;
         
@@ -75,12 +155,44 @@ public class UserInput
         }
 
         
-        return (double) d;
+        return d;
     }
     
+    /***
+     * Return a double from the console with default prompt.
+     */
     public static double getDouble(  ){
-        return getDouble("Input a double:");
+        return getDouble(DOUBLE_INPUT_MESSAGE);
     }
     
+    /***
+     * Return a double from a dialog box showing a custom prompt (and a standard icon)
+     * 
+     * @param   message     A prompt for the user
+     */
+    public static double getDoubleDialog(String message){
+        
+        // get the input and see if it can be made into an integer
+        Double d = null;
+        String error_msg = "Unable to convert input to a number. Try again.\n\n" + message;
+        
+        while(d == null) {
+            try {    
+                String input = JOptionPane.showInputDialog(null, message );
+                d = Double.parseDouble(input);
+            } catch(Exception e) {
+                message = error_msg;
+            }
+        }
+        
+        return (double)d;
+    }
     
+    /***
+     * Return a double from a dialog box showing the default prompt (and a standard icon)
+     */
+    public static double getDoubleDialog(){
+        return getDoubleDialog(DOUBLE_INPUT_MESSAGE);
+    }
+        
 }
